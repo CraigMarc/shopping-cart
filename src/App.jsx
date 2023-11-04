98490f02ac1d4dcf409e4a0612ad27749f2ffc9d
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-11-04 13:46:06 -0400

    cleaned code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Home from './Home'
-import Shop from './Shop'
-import Cart from './Cart'
-import Checkout from './Checkout'
-import ProductPage from './ProductPage'
-import ErrorPage from './ErrorPage'
+import { useState } from 'react'
 import Router from './Router'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from "react-router-dom";
+
 
 function App() {
 
   const [cartItems, setCartItems] = useState([])
   //if api goes down data in assets data
   const [apiItems, setApiItems] = useState()
+
+//send props to router so routes have them available
 
 return (
   <div>
