564e9e59125ea61599a75b5904870f08eae37290
craigmarc <cmarcinkiewicz2000@yahoo.com>
2024-11-13 17:50:10 -0500

    added call to api for shippping price

--- src/Address.jsx
+++ src/Address.jsx
@@ -10,72 +10,28 @@ const Address = () => {
     const [token, setToken] = useState()
     const [loading, setLoading] = useState()
 
-    // get token
-
-
-    const fetchInfo = async () => {
-        //setLoading(true)
-
-        try {
-         
-                const res = await fetch("https://api.usps.com/oauth2/v3/token", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    // body: '{\r\n\t\t"client_id": "{{CLIENT_ID}}",\r\n\t\t"client_secret": "{{CLIENT_SECRET}}",\r\n\t\t"grant_type": "client_credentials"\r\n\t\t}',
-                    body: JSON.stringify({
-                       
-                        
-                    }),
-                    mode: 'no-cors',
-                });
-
-            const tokenData = await res.json();
-
-            setToken(tokenData)
-
-        }
-
-
-        catch (error) {
-            console.error("There has been a problem with your fetch operation:", error);
-            //add error message to dom
-            //setError("true")
-
-        }
-        setLoading(false)
-
-    }
-
-
-    useEffect(() => {
-        fetchInfo();
-    }, [])
-
-    console.log(token)
-
     // submit info for shipping price
 
     const handleSubmit = async e => {
         e.preventDefault();
 
         //send form data
-        await fetch("https://api.usps.com/prices/v3/base-rates/search", {
+        await fetch("http://localhost:3000/users/usps", {
             method: 'POST',
+          //************ add weight and length later from db */
             body: JSON.stringify({
                 originZIPCode: "22407",
-                destinationZIPCode: "63118",
+                destinationZIPCode: zip,
                 weight: 5,
                 length: 3,
                 width: 2,
                 height: 1,
                 mailClass: "USPS_GROUND_ADVANTAGE",
-                processingCategory: "MACHINABLE",
+                processingCategory: "NON_MACHINABLE",
                 rateIndicator: "SP",
                 destinationEntryFacilityType: "NONE",
                 priceType: "COMMERCIAL",
-                mailingDate: "2021-07-01"
+              
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
