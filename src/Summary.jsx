7345a045dd1e6f55c14d380ff1344f84825002e8
craigmarc <cmarcinkiewicz2000@yahoo.com>
2024-11-18 21:10:18 -0500

    added info to summary page

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -8,15 +8,43 @@ const Summary = (props) => {
     cartItems
 
 
-} = props;
+  } = props;
 
-console.log(order)
-    return (
-      <div className="error">
-        <h1>Order Summary</h1>
-     
-      </div>
-    );
-  };
-  
-  export default Summary;
+  let orderTotal = order.price + order.shipping
+
+  console.log(order)
+// add other stuff and fix total when switch api ***********************
+  return (
+    <div className="error">
+      <h1>Order Summary</h1>
+
+      {order.items.map((index) => {
+
+        return (
+
+          <div key={index.id} className="product">
+
+            <div id={index.id} className="card" >
+
+
+              <p>{index.title}</p>
+
+
+            </div>
+
+          </div>
+
+        )
+      })}
+      <p>{order.firstName} {order.lastName}</p>
+      <p>{order.address1}</p>
+      <p>{order.address2}</p>
+      <p>{order.email}</p>
+      <p>subtotal: {order.price}</p>
+      <p>shipping: {order.shipping}</p>
+      <p>order total: {orderTotal}</p>
+    </div>
+  );
+};
+
+export default Summary;
