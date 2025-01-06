e1853731746a540c7d8cb40e86cf45ed4c1e5762
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-01-06 14:17:41 -0500

    added contact email functionality

--- src/components/contactComponents/Contact.jsx
+++ src/components/contactComponents/Contact.jsx
@@ -1,25 +1,102 @@
 import { Header } from '../headerComponents/Header'
+import { useState } from 'react'
+
 
 function Contact(props) {
 
-    const {
+  const {
 
-        cartItems,
-        category,
+    cartItems,
+    category,
+
+  } = props;
+
+  const [message, setMessage] = useState(null)
+
+  //submit function
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    console.log(data.message)
+
+    //send form data
+    await fetch(`http://localhost:3000/users/contact`, {
+      method: 'Post',
+      body: JSON.stringify({
+        subject: data.subject,
+        message: data.message,
+
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+
+    })
+
+
+
+      .then((response) => response.json())
+      .then((data) => {
        
-      } = props;
+       
+        if (data.Email == 'sent') {
+          setMessage("We have recieved your message and will get back to you shortly.")
+        }
+        else {
+          setMessage("Network problem try again later.")
+        }
+      })
 
+
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+
+  }
+
+if (message == null) {
+  return (
+    <div>
+      <Header
+        cartItems={cartItems}
+        category={category}
+      />
+      <div className='contactContainer'>
+      <h3>Contact Us</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Subject</p>
+            <input className="titleInput" type="text" name="subject" />
+          </label>
+          <label>
+            <p>Message</p>
+            <textarea type="text" name="message" />
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        
+      </div>
+     
+    </div>
+  )}
+
+  else {
     return (
-        <div>
-            <Header
-                cartItems={cartItems}
-                category={category}
-            />
-            <div className='contactContainer'>
-            <h3>Contact Us</h3>
-            </div>
-        </div>
+      <div>
+      <Header
+        cartItems={cartItems}
+        category={category}
+      />
+      <div className='contactContainer'>
+        <h3 className='contactMessage'>{message}</h3>
+      </div>
+      </div>
     )
+  }
 }
 
 export default Contact;
